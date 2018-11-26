dc8e3b879ec351e57ecb50ee8b17b1133178da1d
dzmitry.barkevich <dzmitry.barkevich@helmes.com>
2018-11-26 17:19:30 +0300

    upd: replace logger with console output

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -4,11 +4,8 @@ import java.io.File;
 import java.sql.Connection;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Runner {
-    public static final Logger LOG = Logger.getLogger(Runner.class.getName());
     public static final String url = "https://randomfox.ca/floof/";
 
     public static void main(String args[]) {
@@ -16,12 +13,12 @@ public class Runner {
         properties.init();
         Utils utils = new Utils();
         DownloadImage downloadImage = new DownloadImage();
-        LOG.log(Level.INFO, "Getting image link...");
+        System.out.println( "Getting image link...");
         HttpResponse response = downloadImage.executeHttpRequest(url);
         String link = downloadImage.parseImageLink(utils.entityToString(response.getEntity()));
-        LOG.log(Level.INFO, "Image link taken: " + link);
+        System.out.println("Image link taken: " + link);
         File file = downloadImage.downloadAndSave(link, System.getProperty(Constants.IMAGE_FOLDER));
-        LOG.log(Level.INFO, "Image downloaded into folder " + System.getProperty(Constants.IMAGE_FOLDER));
+        System.out.println("Image downloaded into folder " + System.getProperty(Constants.IMAGE_FOLDER));
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new Date());
 
         DatabaseConnection db = new DatabaseConnection();
@@ -29,7 +26,7 @@ public class Runner {
         db.createNewTable(conn);
         db.insertDataIntoDB(conn, link, timeStamp, file.length());
         db.closeConnection(conn);
-        LOG.log(Level.INFO, "Image info saved in DB " + System.getProperty(Constants.DATABASE_NAME));
-        LOG.log(Level.INFO, "Program executed successfully");
+        System.out.println("Image info saved in DB " + System.getProperty(Constants.DATABASE_NAME));
+        System.out.println("Program executed successfully");
     }
 }
